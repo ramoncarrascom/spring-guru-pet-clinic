1423adb49ff5fd8b85d10fac8c47b476370b2c4a
ramoncarrascom <ramon@ramoncarrasco.es>
2020-01-27 20:16:57 +0100

    Implementada BaseEntity. Closes #35

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,8 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     public Long getId() {
         return id;
@@ -12,5 +21,4 @@ public class BaseEntity implements Serializable {
         this.id = id;
     }
 
-    private Long id;
 }
