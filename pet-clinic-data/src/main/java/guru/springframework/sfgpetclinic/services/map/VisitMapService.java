04a14e972d5b0492dde583a2bfeb6ea5fc265848
ramoncarrascom <ramon@ramoncarrasco.es>
2020-01-30 20:32:48 +0100

    Implementados perfiles. Closes #48

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
     @Override
